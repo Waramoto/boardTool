bc028b2afe55d965c4c53d5cbcecb0398eef8445
DyadyaFedor <44328215+DyadyaFedor@users.noreply.github.com>
2021-10-02 14:01:11 +0300

    Update shutdown.sh

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,25 +1,25 @@
 #! /bin/bash 
 
-sudo shutdown +1 
+sudo shutdown +1  # raspberry with screen
 
-ssh pi@192.168.88.244 sudo shutdown now
+ssh pi@192.168.88.231 sudo shutdown now # raspberry 1.1 
 
-ssh pi@192.168.88.243 sudo shutdown now
+ssh pi@192.168.88.222 sudo shutdown now # raspberry 2.1
 
-ssh pi@192.168.88.233 sudo shutdown now
+ssh pi@192.168.88.243 sudo shutdown now # raspberry 3.1 
 
 # "Raspbery Pi down "
 
-ssh fedya@192.168.88.240 -S sudo shutdown now 
-ssh fedya@192.168.88.247 -S sudo shutdown now 
-ssh fedya@192.168.88.250 -S sudo shutdown now
-ssh root@192.168.88.236 -S sudo shutdown now
-ssh fedya@192.168.88.249 -S sudo shutdown now
-ssh fedya@192.168.88.245 -S sudo shutdown now
+ssh fedya@192.168.88.227 -S sudo shutdown now  # orange 1.2
+ssh fedya@192.168.88.217 -S sudo shutdown now  # orange 1.3
+ssh fedya@192.168.88.216 -S sudo shutdown now  # orange 2.2
+ssh fedya@192.168.88.219 -S sudo shutdown now  # orange 2.3
+ssh fedya@192.168.88.218 -S sudo shutdown now  # orange 3.2
+ssh fedya@192.168.88.249 -S sudo shutdown now  # orange 3.3
 
-# " Orange Pi down" 
+# "Orange Pi down" 
 
 ssh admin@192.168.88.2 system shutdown 
 ssh admin@192.168.88.1 system shutdown 
 
-# " Mikrotik down " 
+# "Mikrotik down" 
